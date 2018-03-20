00b2fb7a687026de318ca4a05312bdcea44385a7
Henry Doan <henrydoan96@gmail.com>
2018-03-19 18:31:02 -0600

    can convert words to morse code

---
+++ app.rb
@@ -0,0 +1,38 @@
+@alphabet = {
+  "a" => ".-",
+  "b" => "-...",
+  "c" => "-.-.",
+  "d" => "-..",
+  "e" => ".",
+  "f" => "..-.",
+  "g" => "--.",
+  "h" => "....",
+  "i" => "..",
+  "j" => ".---",
+  "k" => "-.-",
+  "l" => ".-..",
+  "m" => "--",
+  "n" => "-.",
+  "o" => "---",
+  "p" => ".--.",
+  "q" => "--.-",
+  "r" => ".-.",
+  "s" => "...",
+  "t" => "-",
+  "u" => "..-",
+  "v" => "...-",
+  "w" => ".--",
+  "x" => "-..-",
+  "y" => "-.--",
+  "z" => "--.."
+ }
+
+def get_user_input
+  p "Tell me a word to convert to morse code"
+  user_input_arr = gets.strip.chars
+  user_input_arr.each { |letter| 
+    p @alphabet[letter]
+  }
+end
+
+get_user_input
